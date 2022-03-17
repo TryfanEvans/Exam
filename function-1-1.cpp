283d7c57455c9f5189390af0193eb1ddd9cf9eab
TryfanEvans <TryfanLE@gmail.com>
2022-03-17 16:16:56 +1030

    Update function-1-1.cpp

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -3,8 +3,11 @@
 void printer(int array[10][10]) {
     for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
-                std::cout << array[i][j] << " ";
+                std::cout << array[i][j];
+                if (j < 9) {
+                    std::cout << " ";
+                }
 }
-        std::cout << "\b" << std::endl;
+        std::cout << std::endl;
     }
 }
